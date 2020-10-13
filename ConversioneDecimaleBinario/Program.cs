3773c5878f9981877122d9ca297f48d5680b3a61
kevin trentini <trentinikevin6@gmail.com>
2020-10-13 23:15:56 +0200

    Add project files.

---
+++ ConversioneDecimaleBinario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConversioneDecimaleBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, resto;
+            Console.Write("inserisci un numero da convertitre: ");
+            num = int.Parse(Console.ReadLine());
+            string binario = "";
+            while (num > 0)
+            {
+                resto = num % 2;
+                num /= 2;
+                binario = Convert.ToString(resto) + binario;
+
+            }
+            Console.WriteLine($"il numero in binario è {binario}");
+            Console.ReadLine();
+        }
+    }
+}
+
+       
+    
+
